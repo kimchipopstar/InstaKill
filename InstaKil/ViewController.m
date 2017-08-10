1c9782ae6a23e585c9a55f621634689695ed7581
J.K <K_940201@hotmail.com>
2017-08-09 20:22:46 -0700

    complete

--- InstaKil/ViewController.m
+++ InstaKil/ViewController.m
@@ -9,13 +9,21 @@
 #import "ViewController.h"
 #import "ImageCollectionViewCell.h"
 #import "HeaderCollectionReusableView.h"
-#import "FooterCollectionReusableView.h"
+
 
 @interface ViewController () <UICollectionViewDataSource>
 
 @property (strong, nonatomic) NSMutableArray *foodArray;
 @property (strong, nonatomic) NSMutableArray *etcArray;
 @property (strong, nonatomic) NSArray *sectionNames;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *foodOrEct;
+@property (strong, nonatomic) NSMutableArray *homeArray;
+@property (strong, nonatomic) NSMutableArray *outdoorArray;
+@property (strong, nonatomic) NSArray *locationNames;
+@property (nonatomic) UICollectionViewFlowLayout *groupSegment;
+@property (nonatomic) UICollectionViewFlowLayout *locationSegment;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (strong, nonatomic) UICollectionViewLayout *nextLayout;
 
 @end
 
@@ -31,6 +39,34 @@
     
     self.sectionNames = @[@"food",@"ect"];
     
+    self.homeArray = [[NSMutableArray alloc]initWithObjects:[UIImage imageNamed:@"IMG_1645"],[UIImage imageNamed:@"IMG_0061"],[UIImage imageNamed:@"IMG_1637"],[UIImage imageNamed:@"IMG_1604"],[UIImage imageNamed:@"IMG_1628"], nil];
+    
+    self.outdoorArray = [[NSMutableArray alloc]initWithObjects:[UIImage imageNamed:@"IMG_1678"],[UIImage imageNamed:@"IMG_0718"],[UIImage imageNamed:@"IMG_1677"],[UIImage imageNamed:@"IMG_0772"], [UIImage imageNamed:@"IMG_1661"], nil];
+    
+    self.locationNames = @[@"home",@"outdoor"];
+    
+    
+
+    self.groupSegment = [[UICollectionViewFlowLayout alloc] init];
+    self.groupSegment.itemSize = CGSizeMake(100, 100);
+    self.groupSegment.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10);
+    self.groupSegment.minimumInteritemSpacing = 15;
+    self.groupSegment.minimumLineSpacing = 10;
+    self.groupSegment.headerReferenceSize = CGSizeMake(self.collectionView.frame.size.width, 50.0f);
+
+//    self.groupSegment.headerReferenceSize = CGSizeMake(self.collectionView.frame.size.width, 50);
+    //    self.flowLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    
+    self.locationSegment = [[UICollectionViewFlowLayout alloc] init];
+    self.locationSegment.itemSize = CGSizeMake(100, 100);
+    self.locationSegment.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10);
+    self.locationSegment.minimumLineSpacing = 15;
+    self.locationSegment.headerReferenceSize = CGSizeMake(self.collectionView.frame.size.width, 50.0f);
+    self.locationSegment.minimumInteritemSpacing = 10;
+    
+    self.nextLayout = self.groupSegment;
+    [self.collectionView reloadData];
+    
 }
 
 
@@ -39,17 +75,57 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma makr - gestures
+
+- (IBAction)foodOrEct:(UISegmentedControl *)sender {
+    
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            self.nextLayout = self.groupSegment;
+            break;
+            
+        case 1:
+            self.nextLayout = self.locationSegment;
+            
+        default:
+            break;
+    }
+    
+    [self.collectionView.collectionViewLayout invalidateLayout];
+    [self.collectionView reloadData];
+    [self.collectionView setCollectionViewLayout:self.nextLayout animated:YES];
+}
+
+
+
+#pragma makr - collectionView
+
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
    
-    return self.sectionNames.count;
+    if (self.nextLayout == self.groupSegment) {
+        return self.sectionNames.count;
+    } else {
+        return self.locationNames.count;
+    }
+    
+    
 }
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
     
-    if (section == 0) {
+    if (self.nextLayout == self.groupSegment){
+        if (section == 0) {
         return self.foodArray.count;
-    } else {
+        } else {
         return self.etcArray.count;
+        }
+    } else {
+        if (section == 0) {
+            return self.homeArray.count;
+        } else {
+            return self.outdoorArray.count;
+        }
+
     }
     
 }
@@ -57,35 +133,41 @@
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     
     ImageCollectionViewCell *imageCell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ImageCell" forIndexPath:indexPath];
-    
-    if (indexPath.section == 0) {
+    if (self.nextLayout == self.groupSegment) {
+        if (indexPath.section == 0) {
         imageCell.imageCollectionView.image = [self.foodArray objectAtIndex:indexPath.row];
-    } else {
+        } else {
         imageCell.imageCollectionView.image = [self.etcArray objectAtIndex:indexPath.row];
+        }
+    } else {
+        if (indexPath.section == 0) {
+            imageCell.imageCollectionView.image = [self.homeArray objectAtIndex:indexPath.row];
+        } else {
+            imageCell.imageCollectionView.image = [self.outdoorArray objectAtIndex:indexPath.row];
+        }
     }
-    
-    return imageCell;
 
+    return imageCell;
 }
+
+#pragma mark - HeaderCollectionResuable
+
 
 -(UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath{
     
-
-
     if ([kind isEqualToString:UICollectionElementKindSectionHeader]){
         HeaderCollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:@"headerView" forIndexPath:indexPath];
-        headerView.foodSection.text = [self.sectionNames objectAtIndex:0];
         
+        if (self.nextLayout == self.groupSegment){
+            headerView.foodSection.text = [self.sectionNames objectAtIndex:indexPath.section];
+        } else{
+            headerView.foodSection.text = [self.locationNames objectAtIndex:indexPath.section];
+        }
         return headerView;
         
     } else {
-        FooterCollectionReusableView *footerView = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:@"footerView" forIndexPath:indexPath];
-        footerView.etcSection.text = [self.sectionNames objectAtIndex:1];
-        
-        return footerView;
+        return nil;
     }
-
-    
 }
 
 
